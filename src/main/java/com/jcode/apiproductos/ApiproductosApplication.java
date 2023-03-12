d384c8553e03f67f45c4c7e983e036aebe3f6cb9
juschny <97036080+juschny@users.noreply.github.com>
2023-03-12 15:34:50 -0300

    Initial commit

---
+++ src/main/java/com/jcode/apiproductos/ApiproductosApplication.java
@@ -0,0 +1,16 @@
+package com.jcode.apiproductos;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ApiproductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiproductosApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/jcode/apiproductos/ApiproductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcode.apiproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiproductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
